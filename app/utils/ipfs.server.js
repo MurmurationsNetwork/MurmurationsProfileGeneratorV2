dd541b8421c4148bad1bc04b3f1918f6c8eb2792
Ting-Hsuan Wang <vincent@fishboneapps.com>
2022-11-03 16:59:57 -0400

    feat: add Authentication for ipfs (#99)

--- app/utils/ipfs.server.js
+++ app/utils/ipfs.server.js
@@ -1,19 +1,19 @@
-import { fetchFilePost, fetchPost } from '~/utils/fetcher'
+import { fetchFilePostWithAuth, fetchPostWithAuth } from '~/utils/fetcher'
 
 const url = process.env.PRIVATE_IPFS_URL
 
 export async function ipfsKeyGen(arg) {
-  const res = await fetchPost(url + '/key/gen?arg=' + arg)
+  const res = await fetchPostWithAuth(url + '/key/gen?arg=' + arg)
   return await res.json()
 }
 
 export function ipfsPublish(arg, key) {
-  fetchPost(url + '/name/publish?arg=' + arg + '&key=' + key)
+  fetchPostWithAuth(url + '/name/publish?arg=' + arg + '&key=' + key)
 }
 
 export async function ipfsUpload(fileData) {
   let formData = new FormData()
   formData.append('file', fileData)
-  const res = await fetchFilePost(url + '/add?cid-version=1', formData)
+  const res = await fetchFilePostWithAuth(url + '/add?cid-version=1', formData)
   return await res.json()
 }
