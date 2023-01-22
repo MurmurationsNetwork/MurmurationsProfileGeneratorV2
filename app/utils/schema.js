cd1f3c34f3f4a54a0a58cb741ae31f240743a317
Geoff Turk <geoff@ic3.dev>
2023-01-22 22:01:21 +0100

    feat: filter out test schemas

--- app/utils/schema.js
+++ app/utils/schema.js
@@ -8,7 +8,11 @@ export async function loadSchema() {
     })
   }
   let schema = await response.json()
-  return schema.data.filter(s => {
-    return !s.name.startsWith('default-v')
-  })
+  return schema.data
+    .filter(s => {
+      return !s.name.startsWith('default-v')
+    })
+    .filter(s => {
+      return !s.name.startsWith('test_schema-v')
+    })
 }
