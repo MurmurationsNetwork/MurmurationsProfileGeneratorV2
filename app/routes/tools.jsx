014a88c4f9f551683ee2aaa65a12ae5a533deb8c
Geoff Turk <geoff@ic3.dev>
2023-02-10 15:36:41 +0100

    feat: post new or update existing node

--- app/routes/tools.jsx
+++ app/routes/tools.jsx
@@ -3,7 +3,7 @@ import { json } from '@remix-run/node'
 import { useState } from 'react'
 import crypto from 'crypto'
 
-import { getNodeStatus } from '~/utils/index-api'
+import { getNodeStatus, postNode } from '~/utils/index-api'
 
 export async function action({ request }) {
   let formData = await request.formData()
@@ -13,20 +13,30 @@ export async function action({ request }) {
     rawData[key].length === 1 && (rawData[key] = rawData[key][0])
   }
   let { _action, ...data } = rawData
+  let nodeId = crypto
+    .createHash('sha256')
+    .update(data.profile_url)
+    .digest('hex')
   switch (_action) {
-    case 'check':
-      let nodeId = crypto
-        .createHash('sha256')
-        .update(data.profile_url)
-        .digest('hex')
-      let response = await getNodeStatus(nodeId)
-      if (response.errors) {
+    case 'post':
+      let postResponse = await postNode(data.profile_url)
+      if (postResponse.errors) {
         return json({
-          checkErrors: response.errors
+          postErrors: postResponse.errors
         })
       }
       return json({
-        checkResponse: response.data
+        postResponse: postResponse.data
+      })
+    case 'check':
+      let checkResponse = await getNodeStatus(nodeId)
+      if (checkResponse.errors) {
+        return json({
+          checkErrors: checkResponse.errors
+        })
+      }
+      return json({
+        checkResponse: checkResponse.data
       })
     default:
       return null
@@ -46,6 +56,47 @@ export default function Tools() {
         </h1>
       </div>
       <div className="mx-1 md:mx-4">
+        <Form method="post">
+          <label>
+            <h2 className="font-bold text-lg md:text-2xl mb-2 md:mb-4">
+              Add/Update Profile in Index
+            </h2>
+            <input
+              className="form-input w-full md:w-1/2 dark:bg-gray-700 md:mr-2"
+              type="text"
+              name="profile_url"
+              required="required"
+              placeholder="https://your.site/directory/profile.json"
+            />
+          </label>
+          <button
+            className="bg-red-500 dark:bg-purple-200 hover:bg-red-400 dark:hover:bg-purple-100 text-white dark:text-gray-800 font-bold py-2 px-4 w-full md:w-1/3 mt-2 md:mt-0"
+            type="submit"
+            name="_action"
+            value="post"
+            onClick={() => setSubmitType('post')}
+          >
+            {transition.state === 'submitting' && submitType === 'post'
+              ? 'Posting...'
+              : transition.state === 'loading' && submitType === 'post'
+              ? 'Done!'
+              : 'Post Profile'}
+          </button>
+        </Form>
+        <div className="flex flex-auto">
+          {data?.postResponse ? (
+            <div className="bg-green-100 dark:bg-green-700 p-2 my-2 md:p-4 md:my-4 overflow-auto text-sm rounded-xl">
+              <pre>{JSON.stringify(data.postResponse, null, 2)}</pre>
+            </div>
+          ) : null}
+          {data?.postErrors ? (
+            <div className="bg-red-200 dark:bg-red-700 p-2 my-2 md:p-4 md:my-4 overflow-auto text-sm rounded-xl">
+              <pre>{JSON.stringify(data.postErrors, null, 2)}</pre>
+            </div>
+          ) : null}
+        </div>
+      </div>
+      <div className="mx-1 md:mx-4 mt-4 md:mt-8">
         <Form method="post">
           <label>
             <h2 className="font-bold text-lg md:text-2xl mb-2 md:mb-4">
--- app/utils/index-api.js
+++ app/utils/index-api.js
@@ -1,3 +1,16 @@
+export async function postNode(url) {
+  return fetch(process.env.PUBLIC_PROFILE_POST_URL + '/nodes-sync', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: `{"profile_url": "${url}"}`
+  })
+    .then(data => data.json())
+    .catch(err => {
+      console.log('postNode error: ', err)
+      return err
+    })
+}
+
 export async function getNodeStatus(node_id) {
   return fetch(`${process.env.PUBLIC_PROFILE_POST_URL}/nodes/${node_id}`)
     .then(res => res.json())
