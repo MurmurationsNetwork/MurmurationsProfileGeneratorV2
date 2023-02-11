509c63ef9f7da44e77a5fa085bf2f25b68cb8864
Geoff Turk <geoff@ic3.dev>
2023-02-11 19:17:44 +0100

    fix: handle error 3

--- app/root.jsx
+++ app/root.jsx
@@ -30,6 +30,7 @@ export async function loader() {
 
 export default function App() {
   const data = useLoaderData()
+  console.log('data', data)
   return (
     <html lang="en">
       <head>
@@ -40,7 +41,7 @@ export default function App() {
       </head>
       <body className="bg-white dark:bg-gray-900 text-black dark:text-gray-50 leading-normal text-md md:text-xl">
         {process?.env?.NODE_ENV !== 'production' ||
-        process?.env?.VERCEL_ENV !== 'production' ? (
+        window?.env?.VERCEL_ENV !== 'production' ? (
           <div className="flex flex-row bg-fuchsia-200 dark:bg-fuchsia-700 py-1 px-2 md:py-2 md:px-4 h-8 md:h-12 justify-center">
             T E S T &nbsp; E N V I R O N M E N T
           </div>
