9914869181f9ad508724d51f2b2e0987d05b47e0
Geoff Turk <geoff@ic3.dev>
2023-02-11 18:56:10 +0100

    fix: handle error

--- app/root.jsx
+++ app/root.jsx
@@ -28,8 +28,8 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-white dark:bg-gray-900 text-black dark:text-gray-50 leading-normal text-md md:text-xl">
-        {process.env.NODE_ENV !== 'production' ||
-        process.env.VERCEL_ENV !== 'production' ? (
+        {process?.env?.NODE_ENV !== 'production' ||
+        process?.env?.VERCEL_ENV !== 'production' ? (
           <div className="flex flex-row bg-fuchsia-200 dark:bg-fuchsia-700 py-1 px-2 md:py-2 md:px-4 h-8 md:h-12 justify-center">
             T E S T &nbsp; E N V I R O N M E N T
           </div>
